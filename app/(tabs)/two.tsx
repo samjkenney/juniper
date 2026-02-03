158f3817867cc210e6619ac07d17eb8321615543
samjkenney <129895815+samjkenney@users.noreply.github.com>
2026-02-03 17:01:54 -0600

    added data storage

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,13 +1,68 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, TextInput, Button } from 'react-native';
+import {useState} from 'react';
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
 
 export default function TabTwoScreen() {
+  const inputState = {
+    symptom: '',
+    time: '',
+    severity: '',
+    notes: ''
+  };
+
+  const [inputs, setInputs] = useState(inputState);
+
+  async function storeData(key: string, val: string){
+    try {
+      await AsyncStorage.setItem(key, val);
+    } catch (e) {
+
+    }
+  };
+
+  function saveData(){
+    console.log('Saved!');
+    console.log(inputs);
+    const jsonVal = JSON.stringify(inputs);
+    const key = "event-" + inputs['time'];
+    storeData(key, jsonVal);
+  }
+  
+  const handleOnChange = (text: string, input: string) => {
+    setInputs(prevState => ({...prevState, [input]: text}));
+  }
+
+  const getData = async () => {
+    try {
+      const keys = await AsyncStorage.getAllKeys();
+      const result = await AsyncStorage.multiGet(keys);
+      console.log('All keys:', keys);
+      console.log('All results:', result);
+    } catch (e) {
+      console.log('error');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <Text> Symptom: </Text>
+      <TextInput onChangeText={text => handleOnChange(text, 'symptom')}/>
+      <Text> Severity: </Text>
+      <TextInput onChangeText = {text => handleOnChange(text, 'severity')}/>
+      <Text> Time: </Text>
+      <TextInput
+          onChangeText = {text => handleOnChange(text, 'time')}/>
+      <Text> Notes: </Text>
+      <TextInput
+        onChangeText = {text => handleOnChange(text, 'notes')}/>
+      <Button title = "Save" onPress={() => saveData()}/>
+      <Button title = "Get text" onPress={() => getData()}/>
       <EditScreenInfo path="app/(tabs)/two.tsx" />
     </View>
   );
