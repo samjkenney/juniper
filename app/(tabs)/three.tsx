3c3aa5e80ec6778690043cbd7595bffe69dd77d7
samjkenney <129895815+samjkenney@users.noreply.github.com>
2026-02-10 15:24:38 -0600

    Added Tab 3 - display data

---
+++ app/(tabs)/three.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet, TextInput, Button } from 'react-native';
+import {useState} from 'react';
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import DropDownPicker from 'react-native-dropdown-picker';
+
+export default function TabThreeScreen(){
+    const getData = async () => {
+        try {
+            const keys = await AsyncStorage.getAllKeys();
+            const result = await AsyncStorage.multiGet(keys);
+            console.log('All keys:', keys);
+            
+            setkeys(keys.toString());
+            setvalues(result.toString());
+
+            console.log('All results:', result);
+        } catch (e) {
+            console.log('error');
+        }
+    };
+
+    const [keys, setkeys] = useState("");
+    const [values, setvalues] = useState("");
+
+    return(
+        <View>
+            <Text> Hi, this is tab three</Text>
+            <Button title = "Get text" onPress={() => getData()}/>
+            <Text>{ keys }</Text>
+            <Text>{ values }</Text>
+        </View>
+    );
+}
